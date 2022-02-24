0e7cc7057adba8b4ef8184c651ed05d870cb4591
Licht <ahenriquez.4472@unimar.edu.ve>
2022-02-23 21:16:03 -0400

    Revolver y funciones

--- src/ruletaedd/RuletaEDD.java
+++ src/ruletaedd/RuletaEDD.java
@@ -1,17 +1,73 @@
 package ruletaedd;
 import java.io.*;
-class Nodo {
-    String nombre, apellido, cedula;
-    Nodo siguiente = this;
+    class Nodo {
+        String nombre, apellido, cedula;
+        Nodo siguiente = this;
     
     public Nodo(String nom,String ape,String ced){
         this.nombre=nom;
         this.apellido=ape;
         this.cedula=ced;  
+       }
+}
+    class NodoRevolver{
+        boolean bala;
+        NodoRevolver Siguiente;
+        
+    NodoRevolver(){
+        bala = false;
+    }   
+       
+       
+}
+     class arma{
+         NodoRevolver revolver;
+         
+   
+        public void cargarArma(){
+            NodoRevolver aux = revolver;
+            int b = (int) Math.floor(Math.random()*5)+1;
+            for (int i = 0; i < b; i++) {
+            aux = aux.Siguiente;
+        }
+        aux.bala = true;
     }
-     class lista{
+         
+         public arma(){
+             revolver=new NodoRevolver();
+             NodoRevolver aux=revolver;
+      for(int i=0;i>5;i++){
+         aux.Siguiente=new NodoRevolver();
+         aux=aux.Siguiente;
+      }
+        aux.Siguiente=revolver;
+        cargarArma();
+         }
+         
+        public void recargarArma() {
+        NodoRevolver aux = this.revolver;
+        for (int i = 0; i < 6; i++) {
+            aux.bala = false;
+            aux = aux.Siguiente;
+        }
+        cargarArma();
+    } 
+        
+        public boolean disparar() {
+        boolean tiro = revolver.bala;
+        if (tiro) {
+            recargarArma();
+        } else {
+            revolver = revolver.Siguiente;
+        }
+        return tiro;
+    }
+        
+     }
+
+        class listaS{
          Nodo LS;
-         lista(){
+         listaS(){
              LS = null;    
          }
         public void Crear(String nom,String ape,String ced){
@@ -49,13 +105,14 @@ class Nodo {
         
     }
     
-public class RuletaEDD 
-    public static void main(String[] args) throws IOException  {
+
+  
+
+    public class RuletaEDD 
+    public static void main(String[] args) throws IOException{
     
         
 
     }
-  
-}
 
     
